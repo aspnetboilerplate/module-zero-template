971e100d96a3d42f35192caa8596b6e2da06cdd9
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-08-25 22:13:07 +0300

    Upgraded to ABP and module zero v0.6.6.

--- src/AbpCompanyName.AbpProjectName.WebSpaAngular/Abp/Framework/scripts/libs/angularjs/abp.ng.js
+++ src/AbpCompanyName.AbpProjectName.WebSpaAngular/Abp/Framework/scripts/libs/angularjs/abp.ng.js
@@ -56,7 +56,7 @@
                 if (originalData.targetUrl) {
                     abp.ng.http.handleTargetUrl(originalData.targetUrl);
                 }
-            } else { //data.success === false
+            } else if(originalData.success === false) {
                 var messagePromise = null;
 
                 if (originalData.error) {
@@ -116,7 +116,7 @@
 
                         abp.ng.http.logError(error);
 
-                        return $q.reject(error);
+                        return $q.reject(ngError);
                     }
 
                 };
