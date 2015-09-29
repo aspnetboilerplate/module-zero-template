37168a92b774bcb8bc780917e42114341e585f93
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-09-29 13:23:47 +0300

    Refactored template.

--- src/AbpCompanyName.AbpProjectName.WebMpa/App_Start/BundleConfig.cs
+++ src/AbpCompanyName.AbpProjectName.WebMpa/App_Start/BundleConfig.cs
@@ -55,7 +55,8 @@ namespace AbpCompanyName.AbpProjectName.WebMpa
                         "~/Abp/Framework/scripts/libs/abp.jquery.js",
                         "~/Abp/Framework/scripts/libs/abp.toastr.js",
                         "~/Abp/Framework/scripts/libs/abp.blockUI.js",
-                        "~/Abp/Framework/scripts/libs/abp.spin.js"
+                        "~/Abp/Framework/scripts/libs/abp.spin.js",
+                        "~/Abp/Framework/scripts/libs/abp.sweet-alert.js"
                     )
                 );
 
