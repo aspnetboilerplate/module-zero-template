637015abf59d78caa677dce47542b0e8036005fe
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2015-09-29 11:26:35 +0300

    Completed Bearer token auth.

--- src/AbpCompanyName.AbpProjectName.WebApi/Api/Controllers/AccountController.cs
+++ src/AbpCompanyName.AbpProjectName.WebApi/Api/Controllers/AccountController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Abp.Authorization.Users;
+using Abp.UI;
 using Abp.Web.Models;
 using Abp.WebApi.Controllers;
+using AbpCompanyName.AbpProjectName.Api.Models;
+using AbpCompanyName.AbpProjectName.Authorization.Roles;
+using AbpCompanyName.AbpProjectName.MultiTenancy;
 using AbpCompanyName.AbpProjectName.Users;
 using Microsoft.Owin.Infrastructure;
 using Microsoft.Owin.Security;
@@ -25,27 +30,67 @@ namespace AbpCompanyName.AbpProjectName.Api.Controllers
             _userManager = userManager;
         }
 
-        public async Task<AjaxResponse> Authenticate(string username, string password)
+        public async Task<AjaxResponse> Authenticate(LoginModel loginModel)
         {
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-            {
-                return new AjaxResponse(new ErrorInfo("username and password can not be empty"));
-            }
+            CheckModelState();
 
-            var userIdentity = await _userManager.FindAsync(username, password);
-            if (userIdentity == null)
-            {
-                return new AjaxResponse(new ErrorInfo("Invalid user name or password."));
-            }
+            var loginResult = await GetLoginResultAsync(
+                loginModel.UsernameOrEmailAddress,
+                loginModel.Password,
+                loginModel.TenancyName
+                );
 
-            var identity = await _userManager.CreateIdentityAsync(userIdentity, OAuthBearerOptions.AuthenticationType);
-            var ticket = new AuthenticationTicket(identity, new AuthenticationProperties());
+            var ticket = new AuthenticationTicket(loginResult.Identity, new AuthenticationProperties());
 
             var currentUtc = new SystemClock().UtcNow;
             ticket.Properties.IssuedUtc = currentUtc;
             ticket.Properties.ExpiresUtc = currentUtc.Add(TimeSpan.FromMinutes(30));
 
             return new AjaxResponse(OAuthBearerOptions.AccessTokenFormat.Protect(ticket));
+        }
+
+        private async Task<AbpUserManager<Tenant, Role, User>.AbpLoginResult> GetLoginResultAsync(string usernameOrEmailAddress, string password, string tenancyName)
+        {
+            var loginResult = await _userManager.LoginAsync(usernameOrEmailAddress, password, tenancyName);
+
+            switch (loginResult.Result)
+            {
+                case AbpLoginResultType.Success:
+                    return loginResult;
+                default:
+                    throw CreateExceptionForFailedLoginAttempt(loginResult.Result, usernameOrEmailAddress, tenancyName);
+            }
+        }
+
+        private Exception CreateExceptionForFailedLoginAttempt(AbpLoginResultType result, string usernameOrEmailAddress, string tenancyName)
+        {
+            switch (result)
+            {
+                case AbpLoginResultType.Success:
+                    return new ApplicationException("Don't call this method with a success result!");
+                case AbpLoginResultType.InvalidUserNameOrEmailAddress:
+                case AbpLoginResultType.InvalidPassword:
+                    return new UserFriendlyException(L("LoginFailed"), L("InvalidUserNameOrPassword"));
+                case AbpLoginResultType.InvalidTenancyName:
+                    return new UserFriendlyException(L("LoginFailed"), L("ThereIsNoTenantDefinedWithName{0}", tenancyName));
+                case AbpLoginResultType.TenantIsNotActive:
+                    return new UserFriendlyException(L("LoginFailed"), L("TenantIsNotActive", tenancyName));
+                case AbpLoginResultType.UserIsNotActive:
+                    return new UserFriendlyException(L("LoginFailed"), L("UserIsNotActiveAndCanNotLogin", usernameOrEmailAddress));
+                case AbpLoginResultType.UserEmailIsNotConfirmed:
+                    return new UserFriendlyException(L("LoginFailed"), "Your email address is not confirmed. You can not login"); //TODO: localize message
+                default: //Can not fall to default actually. But other result types can be added in the future and we may forget to handle it
+                    Logger.Warn("Unhandled login fail reason: " + result);
+                    return new UserFriendlyException(L("LoginFailed"));
+            }
+        }
+
+        protected virtual void CheckModelState()
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new UserFriendlyException("Invalid request!");
+            }
         }
     }
 }
