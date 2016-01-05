2d0a4369a5ea10423c130a9fd82650dc97bd9186
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-01-05 20:10:29 +0200

    Implemented tenant creation for MPA.

--- src/AbpCompanyName.AbpProjectName.WebMpa/Views/Tenants/Index.js
+++ src/AbpCompanyName.AbpProjectName.WebMpa/Views/Tenants/Index.js
@@ -1,10 +1,29 @@
 ﻿(function() {
     $(function() {
 
-        $('#TenantCreateModalSaveButton').click(function(e) {
-            e.preventDefault();
-            alert('saving...');
-        });
+        var _tenantService = abp.services.app.tenant;
+        var _$modal = $('#TenantCreateModal');
+        var _$form = _$modal.find('form');
 
+        _$form.validate();
+
+        _$form.find('button[type="submit"]').click(function (e) {
+            e.preventDefault();
+
+            if (!_$form.valid()) {
+                return;
+            }
+
+            var tenant = _$form.serializeFormToObject(); //serializeFormToObject is defined in main.js
+            
+            _tenantService.createTenant(tenant).done(function () {
+                _$modal.modal('hide');
+                location.reload(true); //reload page to see new tenant!
+            });
+        });
+        
+        _$modal.on('shown.bs.modal', function () {
+            _$modal.find('input:not([type=hidden]):first').focus();
+        });
     });
 })();
