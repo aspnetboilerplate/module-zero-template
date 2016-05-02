7c6efb48fa041b577296e8b8798a75fb73d87293
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-05-02 16:39:00 +0300

    Implemented MPA users page.

---
+++ src/AbpCompanyName.AbpProjectName.WebMpa/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Abp.Web.Mvc.Authorization;
+using AbpCompanyName.AbpProjectName.Authorization;
+using AbpCompanyName.AbpProjectName.Users;
+
+namespace AbpCompanyName.AbpProjectName.WebMpa.Controllers
+{
+    [AbpMvcAuthorize(PermissionNames.Pages_Users)]
+    public class UsersController : Controller
+    {
+        private readonly IUserAppService _userAppService;
+
+        public UsersController(IUserAppService userAppService)
+        {
+            _userAppService = userAppService;
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var output = await _userAppService.GetUsers();
+            return View(output);
+        }
+    }
+}
