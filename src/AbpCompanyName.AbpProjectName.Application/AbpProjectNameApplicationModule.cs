a2c04493b2e949648bd8cc9d526120ca47048947
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2016-08-24 16:39:48 +0300

    Upgraded to ABP v0.11.

--- src/AbpCompanyName.AbpProjectName.Application/AbpProjectNameApplicationModule.cs
+++ src/AbpCompanyName.AbpProjectName.Application/AbpProjectNameApplicationModule.cs
@@ -7,6 +7,15 @@ namespace AbpCompanyName.AbpProjectName
     [DependsOn(typeof(AbpProjectNameCoreModule), typeof(AbpAutoMapperModule))]
     public class AbpProjectNameApplicationModule : AbpModule
     {
+        public override void PreInitialize()
+        {
+            Configuration.Modules.AbpAutoMapper().Configurators.Add(mapper =>
+            {
+                //Add your custom AutoMapper mappings here...
+                //mapper.CreateMap<,>()
+            });
+        }
+
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
