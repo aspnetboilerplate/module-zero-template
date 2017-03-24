39be31501ec94daa953bf0d43ac65b3c7b144d19
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2017-03-24 14:30:04 +0300

    resolved #38: Don't run multi tenant tests when multi tenancy is disabled.

--- src/Tests/AbpCompanyName.AbpProjectName.Tests/AbpProjectNameTestBase.cs
+++ src/Tests/AbpCompanyName.AbpProjectName.Tests/AbpProjectNameTestBase.cs
@@ -212,8 +212,6 @@ namespace AbpCompanyName.AbpProjectName.Tests
 
         protected void LoginAsHost(string userName)
         {
-            Resolve<IMultiTenancyConfig>().IsEnabled = true;
-
             AbpSession.TenantId = null;
 
             var user =
