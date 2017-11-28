8f7802690ca22a21347e48c903aaa38029299194
Gianpiero Caretti <gpcaretti@gpsoftware.it>
2017-11-28 12:39:38 +0100

    fixed typo: "Persistet" => "Persistent"

--- src/AbpCompanyName.AbpProjectName.WebSpaAngular/App_Start/Startup.cs
+++ src/AbpCompanyName.AbpProjectName.WebSpaAngular/App_Start/Startup.cs
@@ -23,7 +23,7 @@ namespace AbpCompanyName.AbpProjectName.WebSpaAngular
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
                 // evaluate for Persistent cookies (IsPermanent == true). Defaults to 14 days when not set.
-                ExpireTimeSpan = new TimeSpan(int.Parse(ConfigurationManager.AppSettings["AuthSession.ExpireTimeInDays.WhenPersistet"] ?? "14"), 0, 0, 0),
+                ExpireTimeSpan = new TimeSpan(int.Parse(ConfigurationManager.AppSettings["AuthSession.ExpireTimeInDays.WhenPersistent"] ?? "14"), 0, 0, 0),
                 SlidingExpiration = bool.Parse(ConfigurationManager.AppSettings["AuthSession.SlidingExpirationEnabled"] ?? bool.FalseString)
             });
 
