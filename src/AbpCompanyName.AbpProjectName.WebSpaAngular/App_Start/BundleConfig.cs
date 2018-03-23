b126bb877f6302f3f9e9ad0790dcfac2291c6c25
İsmail ÇAGDAŞ <ismailcagdas@gmail.com>
2018-03-23 10:33:13 +0300

    restored nuget packages and added signalr scripts to layouts

--- src/AbpCompanyName.AbpProjectName.WebSpaAngular/App_Start/BundleConfig.cs
+++ src/AbpCompanyName.AbpProjectName.WebSpaAngular/App_Start/BundleConfig.cs
@@ -104,6 +104,7 @@ namespace AbpCompanyName.AbpProjectName.WebSpaAngular
                         "~/Abp/Framework/scripts/libs/angularjs/abp.ng.js",
 
                         "~/js/admin.js",
+                        "~/Scripts/jquery.signalR-2.2.2.min.js",
                         "~/js/main.js"
                     )
                 );
