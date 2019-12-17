ca616ce5090baba666a341ca6b2f92958a173698
Musa Demir <musa.demir@volosoft.com>
2019-12-17 16:42:14 +0300

    merge migrations

--- src/AbpCompanyName.AbpProjectName.EntityFramework/Migrations/201912171340379_Upgraded_To_ABP_5_1_0.Designer.cs
+++ src/AbpCompanyName.AbpProjectName.EntityFramework/Migrations/201912171340379_Upgraded_To_ABP_5_1_0.Designer.cs
@@ -7,13 +7,13 @@ namespace AbpCompanyName.AbpProjectName.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.0")]
-    public sealed partial class Test : IMigrationMetadata
+    public sealed partial class Upgraded_To_ABP_5_1_0 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Test));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Upgraded_To_ABP_5_1_0));
         
         string IMigrationMetadata.Id
         {
-            get { return "201912171252414_Test"; }
+            get { return "201912171340379_Upgraded_To_ABP_5_1_0"; }
         }
         
         string IMigrationMetadata.Source
