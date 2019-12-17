88526569224df1f3ed9df6ff1b5a8df6959b1a8f
maliming <malimings@gmail.com>
2019-12-17 20:53:32 +0800

    Add missing migrations.

---
+++ src/AbpCompanyName.AbpProjectName.EntityFramework/Migrations/201912171252414_Test.cs
@@ -0,0 +1,24 @@
+﻿namespace AbpCompanyName.AbpProjectName.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Test : DbMigration
+    {
+        public override void Up()
+        {
+            DropIndex("dbo.AbpUserLoginAttempts", new[] { "TenancyName", "UserNameOrEmailAddress", "Result" });
+            AlterColumn("dbo.AbpSettings", "Value", c => c.String());
+            AlterColumn("dbo.AbpUserLoginAttempts", "UserNameOrEmailAddress", c => c.String(maxLength: 256));
+            CreateIndex("dbo.AbpUserLoginAttempts", new[] { "TenancyName", "UserNameOrEmailAddress", "Result" });
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.AbpUserLoginAttempts", new[] { "TenancyName", "UserNameOrEmailAddress", "Result" });
+            AlterColumn("dbo.AbpUserLoginAttempts", "UserNameOrEmailAddress", c => c.String(maxLength: 255));
+            AlterColumn("dbo.AbpSettings", "Value", c => c.String(maxLength: 2000));
+            CreateIndex("dbo.AbpUserLoginAttempts", new[] { "TenancyName", "UserNameOrEmailAddress", "Result" });
+        }
+    }
+}
