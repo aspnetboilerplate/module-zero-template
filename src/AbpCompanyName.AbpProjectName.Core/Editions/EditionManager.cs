010e0941a43f9440bd5477af29d57bd5553a38c8
Musa Demir <demirmusa96@gmail.com>
2021-06-28 13:41:31 +0300

    Upgrade to Abp 6.4.0-rc-1

--- src/AbpCompanyName.AbpProjectName.Core/Editions/EditionManager.cs
+++ src/AbpCompanyName.AbpProjectName.Core/Editions/EditionManager.cs
@@ -1,6 +1,7 @@
 ﻿using Abp.Application.Editions;
 using Abp.Application.Features;
 using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
 
 namespace AbpCompanyName.AbpProjectName.Editions
 {
@@ -10,10 +11,12 @@ namespace AbpCompanyName.AbpProjectName.Editions
 
         public EditionManager(
             IRepository<Edition> editionRepository, 
-            IAbpZeroFeatureValueStore featureValueStore)
+            IAbpZeroFeatureValueStore featureValueStore,
+            IUnitOfWorkManager unitOfWorkManager)
             : base(
                 editionRepository,
-                featureValueStore
+                featureValueStore,
+                unitOfWorkManager
             )
         {
         }
